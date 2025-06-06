84c9e733ad1fb44fa06fe2c4cf8d8a0d2761840d
[dyad] <git@dyad.sh>
2025-06-07 02:13:31 +0330

    [dyad] Reverted link text to "Made with Dyad" - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -15,7 +15,7 @@ export const MadeWithDyad = () => {
             rel="noopener noreferrer"
             className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
           >
-            Made with LLM for LLMs!
+            Made with Dyad
           </a>
         </TooltipTrigger>
         <TooltipContent>
