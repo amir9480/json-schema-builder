2fc5bb9b3be58bcaab204a3355c2d2fc76ae9b9f
[dyad] <git@dyad.sh>
2025-06-07 13:45:53 +0330

    [dyad] Removed React Router and set Index as the default page - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,19 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "@/components/theme-provider"; // Import ThemeProvider
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" enableSystem attribute="class"> {/* Add ThemeProvider */}
+    <ThemeProvider defaultTheme="system" enableSystem attribute="class">
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        {/* Render the Index page directly */}
+        <Index />
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
